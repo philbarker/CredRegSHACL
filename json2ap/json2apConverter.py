087e6219942187a7581e372e0bc2d6a0e993d22b
Phil Barker <phil.barker@pjjk.co.uk>
2022-03-11 15:05:59 +0000

    read in some data

--- json2ap/json2apConverter.py
+++ json2ap/json2apConverter.py
@@ -1,5 +1,7 @@
 from AP import AP, PropertyStatement
 from csv import DictReader
+from os.path import exists
+import json
 
 class JSON2APConverter:
     """Class comprising Credential Engine JSON code and python AP, with methods to read the former and convert it into the latter."""
@@ -13,3 +15,24 @@ class JSON2APConverter:
             "ceterms:description",
             "ceterms:subjectWebpage",
         ]
+
+    def read_namespaces(self, filename):
+        """read the namespace prefix:url info from the csv file"""
+        if type(filename) is not str:
+            msg = "filename must be a string"
+            raise TypeError(msg)
+        if not exists(filename):
+            msg = "file named %s does not exist" % filename
+            raise FileNotFoundError(msg)
+        self.ap.load_namespaces(filename)
+
+    def read_json_file(self, filename):
+        if type(filename) is not str:
+            msg = "filename must be a string"
+            raise TypeError(msg)
+        if not exists(filename):
+            msg = "file named %s does not exist" % filename
+            raise FileNotFoundError(msg)
+        with open(filename, "r") as json_file:
+            json_data = json_file.read()
+        self.cr_json = json.loads(json_data)
--- tests/test_json2apConverter.py
+++ tests/test_json2apConverter.py
@@ -1,6 +1,9 @@
 import pytest
 from json2ap import JSON2APConverter
 
+ns_file_name = "inputData/namespaces.csv"
+json_file_name = "inputData/policyBrowserData.json"
+
 @pytest.fixture(scope = "module")
 def test_converter():
     c = JSON2APConverter()
@@ -13,3 +16,26 @@ def test_init(test_converter):
     assert c.ap.namespaces == {}
     assert c.ap.metadata == {}
     assert c.ap.shapeInfo == {}
+
+def test_read_namespaces(test_converter):
+    c = test_converter
+    c.read_namespaces(ns_file_name)
+    assert len(c.ap.namespaces) == 26
+    assert c.ap.namespaces["ceterms"] == "https://purl.org/ctdl/terms/"
+    with pytest.raises(TypeError) as e:
+        c.read_namespaces(2)
+    assert str(e.value) == "filename must be a string"
+    with pytest.raises(FileNotFoundError) as e:
+        c.read_namespaces("not_there.none")
+    assert str(e.value) == "file named not_there.none does not exist"
+
+def test_read_json_file(test_converter):
+    c = test_converter
+    c.read_json_file(json_file_name)
+    assert len(c.cr_json) == 6
+    with pytest.raises(TypeError) as e:
+        c.read_json_file(2)
+    assert str(e.value) == "filename must be a string"
+    with pytest.raises(FileNotFoundError) as e:
+        c.read_json_file("not_there.none")
+    assert str(e.value) == "file named not_there.none does not exist"
