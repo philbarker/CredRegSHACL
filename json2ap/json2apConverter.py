96515d83c38bff79c464939b5ddfbb3c6eb17f0a
Phil Barker <phil.barker@pjjk.co.uk>
2022-03-14 11:29:38 +0000

    convert metadata

--- json2ap/json2apConverter.py
+++ json2ap/json2apConverter.py
@@ -3,8 +3,10 @@ from csv import DictReader
 from os.path import exists
 import json
 
+
 class JSON2APConverter:
     """Class comprising Credential Engine JSON code and python AP, with methods to read the former and convert it into the latter."""
+
     def __init__(self):
         self.ap = AP()
         self.cr_json = dict()
@@ -17,7 +19,7 @@ class JSON2APConverter:
         ]
 
     def read_namespaces(self, filename):
-        """read the namespace prefix:url info from the csv file"""
+        """read the namespace prefix:url info from a csv file"""
         if type(filename) is not str:
             msg = "filename must be a string"
             raise TypeError(msg)
@@ -27,6 +29,7 @@ class JSON2APConverter:
         self.ap.load_namespaces(filename)
 
     def read_json_file(self, filename):
+        """read a JSON file and use it to populate cr_json dict"""
         if type(filename) is not str:
             msg = "filename must be a string"
             raise TypeError(msg)
@@ -36,3 +39,18 @@ class JSON2APConverter:
         with open(filename, "r") as json_file:
             json_data = json_file.read()
         self.cr_json = json.loads(json_data)
+
+    def convert_class(self, class_data, level="required"):
+        if level not in ["required", "recommended", "optional"]:
+            print(level)
+            msg = "Importance level must be one of required, recommended, or optional."
+            raise ValueError(msg)
+        # first the populate the metadata
+        title = class_data["Label"] + " (" + level + ")"
+        description = "%s properties for %s" % (
+            level.capitalize(),
+            class_data["Definition"],
+        )
+        print(description)
+        self.ap.add_metadata("dct:title", title)
+        self.ap.add_metadata("dct:description", description)
--- tests/test_json2apConverter.py
+++ tests/test_json2apConverter.py
@@ -4,10 +4,12 @@ from json2ap import JSON2APConverter
 ns_file_name = "inputData/namespaces.csv"
 json_file_name = "inputData/policyBrowserData.json"
 
-@pytest.fixture(scope = "module")
+
+@pytest.fixture(scope="module")
 def test_converter():
     c = JSON2APConverter()
     return c
+
 
 def test_init(test_converter):
     c = test_converter
@@ -16,6 +18,7 @@ def test_init(test_converter):
     assert c.ap.namespaces == {}
     assert c.ap.metadata == {}
     assert c.ap.shapeInfo == {}
+
 
 def test_read_namespaces(test_converter):
     c = test_converter
@@ -29,13 +32,32 @@ def test_read_namespaces(test_converter):
         c.read_namespaces("not_there.none")
     assert str(e.value) == "file named not_there.none does not exist"
 
+
 def test_read_json_file(test_converter):
     c = test_converter
     c.read_json_file(json_file_name)
     assert len(c.cr_json) == 6
+    assert len(c.cr_json["Policy"]["Classes"]) == 55
     with pytest.raises(TypeError) as e:
         c.read_json_file(2)
     assert str(e.value) == "filename must be a string"
     with pytest.raises(FileNotFoundError) as e:
         c.read_json_file("not_there.none")
     assert str(e.value) == "file named not_there.none does not exist"
+
+
+def test_convert_class(test_converter):
+    c = test_converter
+    class_data = c.cr_json["Policy"]["Classes"][22]
+    c.convert_class(class_data, level="required")
+    assert c.ap.metadata["dct:title"] == "Credential Organization (required)"
+    assert (
+        c.ap.metadata["dct:description"]
+        == "Required properties for Organization that plays one or more key roles in the lifecycle of a credential."
+    )
+    with pytest.raises(ValueError) as e:
+        c.convert_class(class_data, level="important")
+    assert (
+        str(e.value)
+        == "Importance level must be one of required, recommended, or optional."
+    )
