b8abf90a25c45f9ee82b7bfc775cc2ce4b575a6b
Phil Barker <phil.barker@pjjk.co.uk>
2023-06-23 14:53:19 +0100

    Old code. Doesn't work needs updating to current AP class.

--- json2ap/json2apConverter.py
+++ json2ap/json2apConverter.py
@@ -4,6 +4,27 @@ from os.path import exists
 from urllib.parse import quote
 import json
 
+# map importance levels to terms in json data
+levels = {
+    "required": "constraintType:RequiresProperty",
+    "recommended": "constraintType:RecommendsProperty",
+    "optional": "constraintType:OptionalProperty",
+}
+
+# used to decide how to create property constraints
+literals = [
+    "xsd:data",
+    "xsd:string",
+    "rdf:langString",
+    "xsd:boolean",
+    "xsd:integer",
+    "xsd:language",
+    "xsd:float",
+    "xsd:decimal",
+    "xsd:dateTime",
+    "xsd:date",
+]
+
 
 class JSON2APConverter:
     """Class comprising Credential Engine JSON code and python AP, with methods to read the former and convert it into the latter."""
@@ -41,27 +62,49 @@ class JSON2APConverter:
             json_data = json_file.read()
         self.cr_json = json.loads(json_data)
 
-    def convert_class(self, class_data, level="required"):
-        levels = {
-            "required": "constraintType:RequiresProperty",
-            "recommended": "constraintType:RecommendsProperty",
-            "optional": "constraintType:OptionalProperty",
-        }
+    def build_ap(self, class_data, level="required"):
+        """Build an AP based on data about a class and its property sets."""
+        if type(class_data) is not dict:
+            print(class_data)
+            raise TypeError("class data must be a dict.")
+        elif type(level) is not str:
+            print(level)
+            raise TypeError("level must be a string.")
+        self.convert_metadata(class_data, level)
+        self.convert_shape(class_data, level)
+
+    def convert_metadata(self, class_data, level):
+        """Convert metadata about the AP from the data for the first class."""
         if level not in levels.keys():
             print(level)
             msg = "Importance level must be one of required, recommended, or optional."
             raise ValueError(msg)
-        # first the populate the metadata
         title = class_data["Label"] + " (" + level + ")"
         description = "%s properties for %s" % (
             level.capitalize(),
             class_data["Definition"],
         )
-        print(description)
         self.ap.add_metadata("dct:title", title)
         self.ap.add_metadata("dct:description", description)
-        # add top shape for main class
-        shape_id = quote(class_data["Label"].title().replace(" ", "") + "Shape")
+        self.ap.add_metadata("level", level)
+
+    def convert_shape(self, class_data, level, objectOf=None):
+        """Convert class data from JSON into AP ShapeInfo."""
+        if level not in levels.keys():
+            print(level)
+            msg = "Importance level must be one of required, recommended, or optional."
+            raise ValueError(msg)
+        if objectOf is None:
+            shape_id = quote(class_data["Label"].title().replace(" ", "") + "Shape")
+            target = (class_data["URI"], "class")
+        else:
+            property_name = objectOf.split(":")[1]
+            shape_id = quote(
+                property_name.title()
+                + class_data["Label"].title().replace(" ", "")
+                + "Shape"
+            )
+            target = (objectOf, "objectsOf")
         shapeInfo = ShapeInfo()
         shapeInfo.set_id(shape_id)
         shapeInfo.add_label("en", class_data["Label"])
@@ -74,7 +117,120 @@ class JSON2APConverter:
         # add properties for main shape
         for p in class_data["PropertySets"]:
             if p["ImportanceLevel"] == levels[level]:
-                ps = PropertyStatement
-                ps.add_shape(shape_id)
-                ps.add_label("en", p["label"])
-                ps.add_severity("violation")
+                self.convert_property_statement(p, shape_id)
+        return shape_id
+
+    def convert_property_statement(self, property_data, shape_id):
+        """Convert data about a property into PropertyStatement."""
+        # recursively deals with shapes for objects of properties
+        ps = PropertyStatement()
+        ps.add_shape(shape_id)
+        for p in property_data["PropertyURIs"]:
+            ps.add_property(p)
+        ps.add_label("en", property_data["Label"])
+        ps.add_mandatory(True)
+        ps.add_severity("violation")
+        if len(property_data["PropertyURIs"]) == 1:
+            p_uri = property_data["PropertyURIs"][0]
+            range = self.find_range(p_uri)  # returns a list of CURIEs
+            for r in range:
+                if r == "xsd:anyURI":
+                    # handle as rdf:resource with node_type IRI
+                    ps.add_valueNodeType("IRI")
+                    self.ap.add_propertyStatement(ps)
+                elif r in literals:
+                    # add appropriate data
+                    ps.add_valueNodeType("Literal")
+                    ps.add_valueDataType(r)
+                    self.ap.add_propertyStatement(ps)
+                elif r.split(":")[0] in ["ceterms", "schema"]:
+                    (range_data, node_type) = self.get_class_data(r)
+                    level = self.ap.metadata["level"]
+                    if range_data is not None:
+                        shape_id = self.convert_shape(range_data, level, p_uri)
+                        # recurse it baby
+                        # welcome back
+                        ps.add_valueNodeType(node_type)
+                        ps.add_valueShape(shape_id)
+                        self.ap.add_propertyStatement(ps)
+                    else:
+                        # create a shape that enforces only range
+                        self.create_range_shape(p_uri, r)
+                        self.ap.add_propertyStatement(ps)
+                else:
+                    # treat as URI ref,
+                    pass
+        elif len(property_data["PropertyURIs"]) > 1:
+            # need property shapes for each as options
+            pass
+        else:
+            # probably shouldn't get here
+            print(ps)
+            raise ValueError("No property URIs in property set.")
+        return ps  # useful for testing
+
+    def find_range(self, property_uri):
+        if type(property_uri) is not str:
+            print(property_uri)
+            raise TypeError("URI must be passed as a string")
+        elif property_uri.split(":")[0] != "ceterms":
+            print(property_uri)
+            raise ValueError("URI must be CURIE in ceterms namespace")
+        for pd in self.cr_json["PropertyData"]:
+            if pd["URI"] == property_uri:
+                return pd["Range"]
+        raise ValueError("No info on range of property uri " + property_uri)
+
+    def get_class_data(self, class_uri):
+        if type(class_uri) is not str:
+            print(class_uri)
+            raise TypeError("URI must be passed as a string")
+        elif class_uri.split(":")[0] not in ["ceterms", "schema"]:
+            print(class_uri)
+            raise ValueError("URI is CURIE in unrecognised namespace")
+        for c in self.cr_json["Policy"]["Classes"]:
+            if c["URI"] == class_uri:
+                if self.is_ctid_required(c):
+                    node_type = "IRI"
+                else:
+                    node_type = "BNode"
+                return (c, node_type)
+        return (None, None)
+
+    def is_ctid_required(self, class_data):
+        """Return boolean True/False for whether ctid is a required property."""
+        for p in class_data["PropertySets"]:
+            if "ceterms:ctid" in p["PropertyURIs"]:
+                if p["ImportanceLevel"] == "constraintType:RequiresProperty":
+                    return True
+        return False
+
+    def create_range_shape(self, property_uri, range_uri):
+        """Return a shape which expresses range r for property p_uri."""
+        if type(property_uri) is not str:
+            print(property_uri)
+            raise TypeError("URI must be passed as a string")
+        # to do: work out what the shape should be and what to return from this
+        # shape_id will be something like PropertyNameRangeShape
+        # target will be objects of property_uri
+        shapeInfo = ShapeInfo()
+        property_name = property_uri.split(":")[1]
+        range_name = range_uri.split(":")[1]
+        shape_id = quote(property_name.title() + range_name.title() + "Shape")
+        shapeInfo.set_id(shape_id)
+        shapeInfo.add_label("en", "Objects of " + property_uri)
+        shapeInfo.add_comment("en", "Shape for objects of " + property_uri)
+        shapeInfo.append_target(property_uri, "objectsOf")
+        shapeInfo.set_closed("false")
+        shapeInfo.set_mandatory("true")
+        shapeInfo.set_severity("violation")
+        typeProperty = PropertyStatement()
+        typeProperty.add_shape(shape_id)
+        typeProperty.add_property("rdf:type")
+        typeProperty.add_mandatory(True)
+        typeProperty.add_label("en","Specify type property.")
+        typeProperty.add_repeatable(False)
+        typeProperty.add_valueNodeType("iri")
+        typeProperty.add_valueConstraint(range_uri)
+        self.ap.add_propertyStatement(typeProperty)
+        self.ap.add_shapeInfo(shape_id, shapeInfo)
--- tests/test_json2apConverter.py
+++ tests/test_json2apConverter.py
@@ -1,5 +1,6 @@
 import pytest
 from json2ap import JSON2APConverter
+from AP import AP, PropertyStatement, ShapeInfo
 
 ns_file_name = "inputData/namespaces.csv"
 json_file_name = "inputData/policyBrowserData.json"
@@ -46,25 +47,120 @@ def test_read_json_file(test_converter):
     assert str(e.value) == "file named not_there.none does not exist"
 
 
-def test_convert_class(test_converter):
+def test_convert_metadata(test_converter):
     c = test_converter
     class_data = c.cr_json["Policy"]["Classes"][22]
-    c.convert_class(class_data, level="required")
+    c.convert_metadata(class_data, level="required")
     assert c.ap.metadata["dct:title"] == "Credential Organization (required)"
     assert (
         c.ap.metadata["dct:description"]
         == "Required properties for Organization that plays one or more key roles in the lifecycle of a credential."
     )
-    assert (
-        c.ap.shapeInfo["CredentialOrganizationShape"].id
-        == "CredentialOrganizationShape"
-    )
-    assert c.ap.shapeInfo["CredentialOrganizationShape"].targets == {
-        "class": "ceterms:CredentialOrganization"
+
+
+def test_convert_shape(test_converter):
+    c = test_converter
+    class_data = c.cr_json["Policy"]["Classes"][22]
+    c.convert_shape(class_data, "required")
+    info = c.ap.shapeInfo["CredentialOrganizationShape"]
+    assert info.id == "CredentialOrganizationShape"
+    assert info.label == {"en": "Credential Organization"}
+    assert info.comment == {
+        "en": "Shape for organization that plays one or more key roles in the lifecycle of a credential."
     }
+    assert info.targets == {"class": "ceterms:CredentialOrganization"}
+    assert info.closed == False
+    assert info.mandatory == True
+    assert info.severity == "violation"
     with pytest.raises(ValueError) as e:
-        c.convert_class(class_data, level="important")
+        c.build_ap(class_data, level="important")
     assert (
         str(e.value)
         == "Importance level must be one of required, recommended, or optional."
     )
+    c.ap.dump()
+    assert False
+
+
+def test_convert_property_statement(test_converter):
+    c = test_converter
+    agentPurposeData = {
+        "Label": "Agent Purpose",
+        "Definition": "A description of the organization's primary purpose.",
+        "ImportanceLevel": "constraintType:RecommendsProperty",
+        "StatusURI": "vs:stable",
+        "PropertyURIs": ["ceterms:agentPurpose"],
+    }
+    expected_ps = PropertyStatement(
+        shapes=["test_shape"],
+        properties=["ceterms:agentPurpose"],
+        labels={"en": "Agent Purpose"},
+        mandatory=True,
+        repeatable=True,
+        valueNodeTypes=["IRI"],
+        valueDataTypes=[],
+        valueShapes=[],
+        valueConstraints=[],
+        valueConstraintType="",
+        notes={},
+        severity="violation",
+    )
+    ps = c.convert_property_statement(agentPurposeData, "test_shape")
+    assert ps == expected_ps
+    nameData = {
+        "Label": "Name",
+        "Definition": "Name or title of the resource.",
+        "ImportanceLevel": "constraintType:RequiresProperty",
+        "StatusURI": "vs:stable",
+        "PropertyURIs": ["ceterms:name"],
+    }
+    expected_ps = PropertyStatement(
+        shapes=["test_shape"],
+        properties=["ceterms:name"],
+        labels={"en": "Name"},
+        mandatory=True,
+        repeatable=True,
+        valueNodeTypes=["Literal"],
+        valueDataTypes=["rdf:langString"],
+        valueShapes=[],
+        valueConstraints=[],
+        valueConstraintType="",
+        notes={},
+        severity="violation",
+    )
+    ps = c.convert_property_statement(nameData, "test_shape")
+    assert ps == expected_ps
+    accreditedInData = {
+        "Label": "Accredited In",
+        "Definition": "Region or political jurisdiction such as a state, province or locale in which the credential, learning opportunity or assessment is accredited.",
+        "Comment": "",
+        "ImportanceLevel": "constraintType:RequiresProperty",
+        "StatusURI": "vs:stable",
+        "PropertyURIs": ["ceterms:accreditedIn"],
+    }
+    expected_ps = PropertyStatement(
+        shapes=["test_shape"],
+        properties=["ceterms:accreditedIn"],
+        labels={"en": "Accredited In"},
+        mandatory=True,
+        repeatable=True,
+        valueNodeTypes=[],  # to fix, should be BNode / BNode or IRI
+        valueDataTypes=[],
+        valueShapes=[],  # to fix, enforce range
+        valueConstraints=[],
+        valueConstraintType="",
+        notes={},
+        severity="violation",
+    )
+    ps = c.convert_property_statement(accreditedInData, "test_shape")
+    assert ps == expected_ps
+
+
+def test_get_class_data(test_converter):
+    c = test_converter
+    (cd, node_type) = c.get_class_data("ceterms:ApprenticeshipCertificate")
+    assert node_type == "IRI"
+    assert cd["Label"] == "Apprenticeship Certificate"
+    (cd, node_type) = c.get_class_data("schema:QuantitativeValue")
+    assert node_type == "BNode"
+    assert cd["Label"] == "Quantitative Value"
